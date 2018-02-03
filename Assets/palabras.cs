f7334a3846fccf12e58556ab3dd13eeeaad1fb23
Stalin Carrión <sfcarrion@utpl.edu.ec>
2018-02-03 12:12:06 -0500

    todo los scribs que se necesita para abrir el proyecto :fire:

---
+++ Assets/palabras.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class palabras : MonoBehaviour {
+    public InputField texbox1;
+    public string a ;
+    
+	// Use this for initialization
+	void Start () {
+        a = "a";
+        texbox1.text.Replace("aaaaa", "\n"); 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AutomataCompiladores
+Para poder ejecutar el archivo, se debe crear un nuevo proyecto con el mismo nombre y solo copiar y pegar el Assets
